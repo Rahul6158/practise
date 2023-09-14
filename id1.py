a3f88f6c6101c831688cf72c876f4f2b0886a30c
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-14 20:10:04 +0530

    Update id1.py

--- id1.py
+++ id1.py
@@ -1,15 +1,23 @@
 import streamlit as st
-import doc2txt
+import docx
 
-def extract_text_from_docx(docx_file):
+def extract_bullets_from_docx(docx_file):
     try:
-        text = doc2txt.extract_text(docx_file)
-        return text
+        doc = docx.Document(docx_file)
+
+        bullet_points = []
+
+        for paragraph in doc.paragraphs:
+            if paragraph.style.name.startswith('List Bullet'):
+                bullet_points.append(paragraph.text)
+
+        return bullet_points
+
     except Exception as e:
         return str(e)
 
 def main():
-    st.title("DOCX Text Extractor")
+    st.title("Bullet Point Extractor from DOCX")
 
     uploaded_file = st.file_uploader("Upload a DOCX file", type=["docx"])
 
@@ -17,14 +25,14 @@ def main():
         st.markdown("### Uploaded DOCX File:")
         st.write(uploaded_file.name)
 
-        if st.button("Extract Text"):
-            extracted_text = extract_text_from_docx(uploaded_file)
+        bullet_points = extract_bullets_from_docx(uploaded_file)
 
-            if extracted_text:
-                st.markdown("### Extracted Text:")
-                st.write(extracted_text)
-            else:
-                st.error("Error: Unable to extract text from the DOCX file.")
+        if bullet_points:
+            st.markdown("### Extracted Bullet Points:")
+            for i, bullet_point in enumerate(bullet_points, start=1):
+                st.write(f"{i}. {bullet_point}")
+        else:
+            st.warning("No bullet points found in the DOCX file.")
 
 if __name__ == "__main__":
     main()
