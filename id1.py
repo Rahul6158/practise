209f21091beaea382ccc2f752fd1974abeb59c03
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-17 16:32:24 +0530

    Update id1.py

--- id1.py
+++ id1.py
@@ -1,211 +1,38 @@
 import streamlit as st
-import os
-import base64
-import docx2txt
-from googletrans import Translator
-from gtts import gTTS
-import io
-from docx import Document
-from bs4 import BeautifulSoup
+import docx
 
-# Function to extract text from a DOCX file
-def process_docx_text(docx_file, skip_lists=False):
-    # Extract text from the DOCX file
-    if skip_lists:
-        # Use custom function to remove lists
-        text = process_docx_text_without_lists(docx_file)
-    else:
-        text = docx2txt.process(docx_file)
-    return text
+def extract_bullets_from_docx(docx_file):
+    try:
+        doc = docx.Document(docx_file)
 
-# Custom function to remove lists from DOCX text
-def process_docx_text_without_lists(docx_file):
-    doc = Document(docx_file)
-    text = ""
-    for paragraph in doc.paragraphs:
-        if not paragraph.style.name.startswith('List'):
-            text += paragraph.text + '\n'
-    return text
+        bullet_points = []
 
-# Function to translate text using Google Translate API
-def translate_text_google(text, target_language):
-    translator = Translator()
-    translated_text = translator.translate(text, dest=target_language)
-    return translated_text.text
+        for paragraph in doc.paragraphs:
+            if paragraph.style.name.startswith('List Bullet'):
+                bullet_points.append(paragraph.text)
 
-# Function to convert text to speech and save as an MP3 file
-def convert_text_to_speech(text, output_file, language='en'):
-    if text:
-        tts = gTTS(text=text, lang=language)
-        tts.save(output_file)
+        return bullet_points
 
-# Function to generate a download link for a file
-def get_binary_file_downloader_html(link_text, file_path, file_format):
-    with open(file_path, 'rb') as f:
-        file_data = f.read()
-    b64_file = base64.b64encode(file_data).decode()
-    download_link = f'<a href="data:{file_format};base64,{b64_file}" download="{os.path.basename(file_path)}">{link_text}</a>'
-    return download_link
+    except Exception as e:
+        return str(e)
 
-# Function to convert translated text to a Word document
-def convert_text_to_word_doc(text, output_file):
-    doc = Document()
-    doc.add_paragraph(text)
-    doc.save(output_file)
-
-# Function to convert Word document to HTML
-def convert_word_doc_to_html(docx_file):
-    txt = docx2txt.process(docx_file)
-    soup = BeautifulSoup(txt, 'html.parser')
-    return soup.prettify()
-
-language_mapping = {
-    "en": "English",
-    "es": "Spanish",
-    "fr": "French",
-    "de": "German",
-    "it": "Italian",
-    "pt": "Portuguese",
-    "nl": "Dutch",
-    "hi": "Hindi",
-    "ja": "Japanese",
-    "ko": "Korean",
-    "zh-cn": "Simplified Chinese",
-    "ru": "Russian",
-    "ar": "Arabic",
-    "th": "Thai",
-    "tr": "Turkish",
-    "pl": "Polish",
-    "cs": "Czech",
-    "sv": "Swedish",
-    "da": "Danish",
-    "fi": "Finnish",
-    "el": "Greek",
-    "hu": "Hungarian",
-    "uk": "Ukrainian",
-    "no": "Norwegian",
-    "id": "Indonesian",
-    "vi": "Vietnamese",
-    "ro": "Romanian",
-    "bn": "Bengali",
-    "fa": "Persian",
-    "iw": "Hebrew",
-    "bg": "Bulgarian",
-    "ca": "Catalan",
-    "hr": "Croatian",
-    "sr": "Serbian",
-    "sk": "Slovak",
-    "sl": "Slovenian",
-    "lt": "Lithuanian",
-    "lv": "Latvian",
-    "et": "Estonian",
-    "is": "Icelandic",
-    "ga": "Irish",
-    "sq": "Albanian",
-    "mk": "Macedonian",
-    "hy": "Armenian",
-    "ka": "Georgian",
-    "mt": "Maltese",
-    "mr": "Marathi",
-    "ta": "Tamil",
-    "te": "Telugu",
-    "ur": "Urdu",
-    "ne": "Nepali",
-    "si": "Sinhala",
-    "km": "Khmer",
-    "lo": "Lao",
-    "my": "Burmese",
-    "jw": "Javanese",
-    "mn": "Mongolian",
-    "zu": "Zulu",
-    "xh": "Xhosa"
-}
-
-# Main Streamlit app
 def main():
-    st.image("jangirii.png", width=50)
-    st.title("Text Translation and Conversion to Speech (English - other languages)")
+    st.title("Bullet Point Extractor from DOCX")
 
-    # Add a file uploader for DOCX files
-    uploaded_docx = st.file_uploader("Upload a DOCX file", type=["docx"])
+    uploaded_file = st.file_uploader("Upload a DOCX file", type=["docx"])
 
-    if uploaded_docx is not None:
-        # Read the uploaded DOCX file and process its text content
-        docx_text = process_docx_text(uploaded_docx)
+    if uploaded_file is not None:
+        st.markdown("### Uploaded DOCX File:")
+        st.write(uploaded_file.name)
 
-        # Display the extracted text
-        st.subheader("Text Extracted from Uploaded DOCX:")
-        st.write(docx_text)
+        bullet_points = extract_bullets_from_docx(uploaded_file)
 
-        target_language = st.selectbox("Select target language:", list(language_mapping.values()))
-
-        # Check if the target language is in the mapping
-        target_language_code = [code for code, lang in language_mapping.items() if lang == target_language][0]
-
-        # Translate the extracted text using Google Translate
-        try:
-            translated_text = translate_text_google(docx_text, target_language_code)
-        except Exception as e:
-            st.error(f"Translation error: {str(e)}")
-            translated_text = None
-
-
-        # Display translated text
-        if translated_text:
-            st.subheader(f"Translated text ({target_language}):")
-            st.write(translated_text)
+        if bullet_points:
+            st.markdown("### Extracted Bullet Points:")
+            for i, bullet_point in enumerate(bullet_points, start=1):
+                st.write(f"{i}. {bullet_point}")
         else:
-            st.warning("Translation result is empty. Please check your input text.")
-
-        # Convert the translated text to speech
-        if st.button("Convert to Speech"):
-            output_file = "translated_speech.mp3"
-            convert_text_to_speech(translated_text, output_file, language=target_language_code)
-
-            # Play the generated speech
-            audio_file = open(output_file, 'rb')
-            st.audio(audio_file.read(), format='audio/mp3')
-
-            # Play the generated speech (platform-dependent)
-            if os.name == 'posix':  # For Unix/Linux
-                os.system(f"xdg-open {output_file}")
-            elif os.name == 'nt':  # For Windows
-                os.system(f"start {output_file}")
-            else:
-                st.warning("Unsupported operating system")
-
-            # Provide a download link for the MP3 file
-            st.markdown(get_binary_file_downloader_html("Download Audio File", output_file, 'audio/mp3'), unsafe_allow_html=True)
-
-            # Convert the translated text to a Word document
-            word_output_file = "translated_text.docx"
-            convert_text_to_word_doc(translated_text, word_output_file)
-
-            # Display the Word document as HTML
-            with open(word_output_file, "rb") as f:
-                doc_content = convert_word_doc_to_html(f)
-                html_data = f"""
-            <!DOCTYPE html>
-            <html>
-            <head>
-            <style>
-            /* Define a CSS class for text with a specific color */
-            .custom-text {{
-                color: white; /* Change 'white' to the desired color */
-            }}
-            </style>
-            </head>
-            <body>
-            <!-- Apply the CSS class to the Word document content -->
-            <p class="custom-text">{doc_content}</p>
-            </body>
-            </html>
-            """
-                st.subheader("Preview of Translated Text as Word Document:")
-                st.components.v1.html(html_data, width=600, height=800)
-            
-            # Provide a download link for the Word document
-                st.markdown(get_binary_file_downloader_html("Download Word Document", word_output_file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
+            st.warning("No bullet points found in the DOCX file.")
 
 if __name__ == "__main__":
     main()
