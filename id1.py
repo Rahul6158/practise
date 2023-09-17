4951390de3ac87028eaf82298e62c9872f25998d
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-17 17:33:57 +0530

    Update id1.py

--- id1.py
+++ id1.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 import base64
+import docx2txt
 from googletrans import Translator
 from gtts import gTTS
 import io
@@ -78,12 +79,66 @@ language_mapping = {
     "en": "English",
     "es": "Spanish",
     "fr": "French",
-    # Add more language codes and names here
+    "de": "German",
+    "it": "Italian",
+    "pt": "Portuguese",
+    "nl": "Dutch",
+    "hi": "Hindi",
+    "ja": "Japanese",
+    "ko": "Korean",
+    "zh-cn": "Simplified Chinese",
+    "ru": "Russian",
+    "ar": "Arabic",
+    "th": "Thai",
+    "tr": "Turkish",
+    "pl": "Polish",
+    "cs": "Czech",
+    "sv": "Swedish",
+    "da": "Danish",
+    "fi": "Finnish",
+    "el": "Greek",
+    "hu": "Hungarian",
+    "uk": "Ukrainian",
+    "no": "Norwegian",
+    "id": "Indonesian",
+    "vi": "Vietnamese",
+    "ro": "Romanian",
+    "bn": "Bengali",
+    "fa": "Persian",
+    "iw": "Hebrew",
+    "bg": "Bulgarian",
+    "ca": "Catalan",
+    "hr": "Croatian",
+    "sr": "Serbian",
+    "sk": "Slovak",
+    "sl": "Slovenian",
+    "lt": "Lithuanian",
+    "lv": "Latvian",
+    "et": "Estonian",
+    "is": "Icelandic",
+    "ga": "Irish",
+    "sq": "Albanian",
+    "mk": "Macedonian",
+    "hy": "Armenian",
+    "ka": "Georgian",
+    "mt": "Maltese",
+    "mr": "Marathi",
+    "ta": "Tamil",
+    "te": "Telugu",
+    "ur": "Urdu",
+    "ne": "Nepali",
+    "si": "Sinhala",
+    "km": "Khmer",
+    "lo": "Lao",
+    "my": "Burmese",
+    "jw": "Javanese",
+    "mn": "Mongolian",
+    "zu": "Zulu",
+    "xh": "Xhosa"
 }
 
 # Main Streamlit app
 def main():
-    st.image("jangirii.png", width=50)
     st.title("Text Translation and Conversion to Speech (English - other languages)")
 
     # Add a file uploader for DOCX files
@@ -108,7 +163,7 @@ def main():
         except Exception as e:
             st.error(f"Translation error: {str(e)}")
             translated_text = None
-    
+
         # Display translated text
         if translated_text:
             st.subheader(f"Translated text ({target_language}):")
@@ -139,6 +194,7 @@ def main():
             # Convert the translated text to a Word document
             word_output_file = "translated_text.docx"
             convert_text_to_word_doc(translated_text, word_output_file)
+            
             # Provide a download link for the Word document
             st.markdown(get_binary_file_downloader_html("Download Word Document", word_output_file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
 
