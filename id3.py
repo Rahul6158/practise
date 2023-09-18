b1c19d479cf74ce03eaeac6a0bb8a846f9166fec
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-18 11:24:45 +0530

    Update id3.py

--- id3.py
+++ id3.py
@@ -1,33 +1,43 @@
 import streamlit as st
 import docx
-from docx.shared import Length
 import base64
 import os
+
+# Function to check and set the structure of the DOCX file for processing
+def set_doc_structure(doc):
+    new_doc = docx.Document()
+
+    for element in doc.element.body:
+        if isinstance(element, docx.oxml.text.paragraph.CT_P):
+            # Process paragraphs and their runs
+            for run in element.runs:
+                if run.text.strip():  # Check if the text content is non-empty
+                    new_paragraph = new_doc.add_paragraph()
+                    new_run = new_paragraph.add_run(run.text)
+                    new_run.bold = run.bold
+                    new_run.italic = run.italic
+                    new_run.underline = run.underline
+                    new_run.font.size = run.font.size
+                    new_run.font.color.rgb = run.font.color.rgb
+                    # You can copy other run properties as needed
+        elif not isinstance(element, docx.oxml.text.Run):
+            # If the element is not a paragraph, create an empty paragraph
+            new_doc.add_paragraph()
+
+    return new_doc
 
 # Function to remove non-string objects from a DOCX file
 def remove_non_string_objects(docx_file):
     try:
         doc = docx.Document(docx_file)
-        new_doc = docx.Document()
 
-        for element in doc.element.body:
-            if isinstance(element, docx.oxml.text.paragraph.CT_P):
-                # Process paragraphs and their runs
-                for run in element.runs:
-                    if run.text.strip():  # Check if the text content is non-empty
-                        new_paragraph = new_doc.add_paragraph()
-                        new_run = new_paragraph.add_run(run.text)
-                        new_run.bold = run.bold
-                        new_run.italic = run.italic
-                        new_run.underline = run.underline
-                        new_run.font.size = run.font.size
-                        new_run.font.color.rgb = run.font.color.rgb
-                        # You can copy other run properties as needed
+        # Set the document structure to a convenient format
+        new_doc = set_doc_structure(doc)
 
         # Save the modified document to a new file
         output_file_path = "document_without_non_string.docx"
         new_doc.save(output_file_path)
-        
+
         return output_file_path
 
     except Exception as e:
