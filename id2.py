8d63dd3a2c326ac02ee00d315a1359b688803465
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-18 22:18:36 +0530

    Update id2.py

--- id2.py
+++ id2.py
@@ -1,148 +1,159 @@
 import streamlit as st
+import os
+import base64
+import docx2txt
+from googletrans import Translator
+from gtts import gTTS
 import io
+from docx import Document
+from bs4 import BeautifulSoup
 from PIL import Image
 import pytesseract
 import PyPDF2
-import docx2txt
-from translate import Translator
 
-# Language mapping
-language_mapping = {
-    "en": "English",
-    "es": "Spanish",
-    "fr": "French",
-    "de": "German",
-    "it": "Italian",
-    "pt": "Portuguese",
-    "nl": "Dutch",
-    "hi": "Hindi",
-    "ja": "Japanese",
-    "ko": "Korean",
-    "zh-cn": "Simplified Chinese",
-    "ru": "Russian",
-    "ar": "Arabic",
-    "th": "Thai",
-    "tr": "Turkish",
-    "pl": "Polish",
-    "cs": "Czech",
-    "sv": "Swedish",
-    "da": "Danish",
-    "fi": "Finnish",
-    "el": "Greek",
-    "hu": "Hungarian",
-    "uk": "Ukrainian",
-    "no": "Norwegian",
-    "id": "Indonesian",
-    "vi": "Vietnamese",
-    "ro": "Romanian",
-    "bn": "Bengali",
-    "fa": "Persian",
-    "iw": "Hebrew",
-    "bg": "Bulgarian",
-    "ca": "Catalan",
-    "hr": "Croatian",
-    "sr": "Serbian",
-    "sk": "Slovak",
-    "sl": "Slovenian",
-    "lt": "Lithuanian",
-    "lv": "Latvian",
-    "et": "Estonian",
-    "is": "Icelandic",
-    "ga": "Irish",
-    "sq": "Albanian",
-    "mk": "Macedonian",
-    "hy": "Armenian",
-    "ka": "Georgian",
-    "mt": "Maltese",
-    "mr": "Marathi",
-    "ta": "Tamil",
-    "te": "Telugu",
-    "ur": "Urdu",
-    "ne": "Nepali",
-    "si": "Sinhala",
-    "km": "Khmer",
-    "lo": "Lao",
-    "my": "Burmese",
-    "jw": "Javanese",
-    "mn": "Mongolian",
-    "zu": "Zulu",
-    "xh": "Xhosa"
-}
-
-# Custom function to process DOCX text without lists
-def process_docx_text_without_lists(docx_file):
-    text = docx2txt.process(docx_file)
-    
-    # Remove list items
-    text = '\n'.join([line for line in text.split('\n') if not line.startswith(('- ', '• ', 'o '))])
-    
+# Function to extract text from a DOCX file
+def process_docx_text(docx_file, skip_lists=True):
+    # Extract text from the DOCX file
+    if skip_lists:
+        # Use custom function to remove lists
+        text = process_docx_text_without_lists(docx_file)
+    else:
+        text = docx2txt.process(docx_file)
     return text
 
-# Custom function to process PDF text without lists
-def process_pdf_text_without_lists(pdf_file):
+# Custom function to remove lists from DOCX text
+def process_docx_text_without_lists(docx_file):
+    doc = Document(docx_file)
+    text = ""
+    for paragraph in doc.paragraphs:
+        if not paragraph.style.name.startswith('•'):
+            text += paragraph.text + '\n'
+    return text
+
+# Function to translate text using Google Translate API
+def translate_text_google(text, target_language):
+    translator = Translator()
+    translated_text = translator.translate(text, dest=target_language)
+    return translated_text.text
+
+# Function to convert text to speech and save as an MP3 file
+def convert_text_to_speech(text, output_file, language='en'):
+    if text:
+        tts = gTTS(text=text, lang=language)
+        tts.save(output_file)
+
+# Function to generate a download link for a file
+def get_binary_file_downloader_html(link_text, file_path, file_format):
+    with open(file_path, 'rb') as f:
+        file_data = f.read()
+    b64_file = base64.b64encode(file_data).decode()
+    download_link = f'<a href="data:{file_format};base64,{b64_file}" download="{os.path.basename(file_path)}">{link_text}</a>'
+    return download_link
+
+# Function to convert translated text to a Word document
+def convert_text_to_word_doc(text, output_file):
+    doc = Document()
+    doc.add_paragraph(text)
+    doc.save(output_file)
+
+# Function to convert Word document to HTML
+def convert_word_doc_to_html(docx_file):
+    txt = docx2txt.process(docx_file)
+    soup = BeautifulSoup(txt, 'html.parser')
+    return soup.prettify()
+
+# Function to extract text from an image
+def extract_text_from_image(img):
+    text = pytesseract.image_to_string(img)
+    return text
+
+# Function to extract text from a PDF file
+def process_pdf_text(pdf_file):
     pdf_text = ""
     pdf_reader = PyPDF2.PdfReader(pdf_file)
     for page_num in range(len(pdf_reader.pages)):
         page = pdf_reader.pages[page_num]
         pdf_text += page.extract_text()
-    
-    # Remove list items
-    pdf_text = '\n'.join([line for line in pdf_text.split('\n') if not line.startswith(('- ', '• ', 'o '))])
-    
     return pdf_text
 
-# Custom function to extract text from an image
-def extract_text_from_image(img):
-    text = pytesseract.image_to_string(img)
-    return text
+# Main Streamlit app
+def main():
+    st.image("jangirii.png", width=50)
+    st.title("Text Translation and Conversion to Speech (English - other languages)")
 
-# Create a Streamlit app
-st.title("File Uploader with OCR and Translation")
+    # Add a file uploader for various file types
+    uploaded_file = st.file_uploader("Upload a file", type=["docx", "pdf", "jpg", "jpeg", "png"])
 
-# Create an uploader widget
-uploaded_file = st.file_uploader("Upload a file", type=["docx", "pdf", "jpg", "jpeg", "png", "txt"])
-
-if uploaded_file is not None:
-    file_extension = uploaded_file.name.split('.')[-1].lower()
-    if file_extension == "docx":
-        # Display DOCX content
-        docx_text = process_docx_text_without_lists(uploaded_file)
-        st.write(docx_text)
-    elif file_extension == "pdf":
-        # Display PDF content
-        pdf_text = process_pdf_text_without_lists(uploaded_file)
-        st.write(pdf_text)
-    elif file_extension in ["jpg", "jpeg", "png"]:
-        # Display image
-        img = Image.open(uploaded_file)
-        st.image(img, caption="Uploaded Image", use_column_width=True)
+    if uploaded_file is not None:
+        file_extension = uploaded_file.name.split('.')[-1].lower()
         
-        # Extract text from the image using custom function
-        text = extract_text_from_image(img)
-        st.write("Text extracted from the image:")
-        st.write(text)
-    elif file_extension == "txt":
-        # Display TXT content
-        txt_text = uploaded_file.read()
-        st.write(txt_text)
+        if file_extension == "docx":
+            # Display DOCX content
+            docx_text = process_docx_text(uploaded_file)
+            st.write(docx_text)
+        elif file_extension == "pdf":
+            # Display PDF content
+            pdf_text = process_pdf_text(uploaded_file)
+            st.write(pdf_text)
+        elif file_extension in ["jpg", "jpeg", "png"]:
+            # Display image
+            img = Image.open(uploaded_file)
+            st.image(img, caption="Uploaded Image", use_column_width=True)
+            
+            # Extract text from the image using pytesseract
+            text = extract_text_from_image(img)
+            st.write("Text extracted from the image:")
+            st.write(text)
 
-    # Language translation
-    translation_language = st.selectbox("Select a language for translation:", options=list(language_mapping.values()))
-    target_language = [key for key, value in language_mapping.items() if value == translation_language][0]
-    
-    if st.button("Translate"):
-        # Split the text into sentences and translate each sentence
-        sentences = text.split('.')
-        translated_sentences = []
+        target_language = st.selectbox("Select target language:", options=list(language_mapping.values()))
 
-        for sentence in sentences:
-            translator = Translator(to_lang=target_language)
-            translated_sentence = translator.translate(sentence)
-            translated_sentences.append(translated_sentence)
+        # Check if the target language is in the mapping
+        target_language_code = [code for code, lang in language_mapping.items() if lang == target_language][0]
 
-        translated_text = '. '.join(translated_sentences)
-        st.write(f"Translated text to {translation_language}:")
-        st.write(translated_text)
+        # Translate the extracted text using Google Translate
+        try:
+            if file_extension == "docx":
+                translated_text = translate_text_google(docx_text, target_language_code)
+            elif file_extension == "pdf":
+                translated_text = translate_text_google(pdf_text, target_language_code)
+            elif file_extension in ["jpg", "jpeg", "png"]:
+                translated_text = translate_text_google(text, target_language_code)
+        except Exception as e:
+            st.error(f"Translation error: {str(e)}")
+            translated_text = None
 
-# Add some instructions for the user
-st.write("Supported file types: DOCX, PDF, JPG/JPEG, PNG, TXT")
+        # Display translated text
+        if translated_text:
+            st.subheader(f"Translated text ({target_language}):")
+            st.write(translated_text)
+        else:
+            st.warning("Translation result is empty. Please check your input text.")
+
+        # Convert the translated text to speech
+        if st.button("Convert to Speech and get Translated document"):
+            output_file = "translated_speech.mp3"
+            convert_text_to_speech(translated_text, output_file, language=target_language_code)
+
+            # Play the generated speech
+            audio_file = open(output_file, 'rb')
+            st.audio(audio_file.read(), format='audio/mp3')
+
+            # Play the generated speech (platform-dependent)
+            if os.name == 'posix':  # For Unix/Linux
+                os.system(f"xdg-open {output_file}")
+            elif os.name == 'nt':  # For Windows
+                os.system(f"start {output_file}")
+            else:
+                st.warning("Unsupported operating system")
+
+            # Provide a download link for the MP3 file
+            st.markdown(get_binary_file_downloader_html("Download Audio File", output_file, 'audio/mp3'), unsafe_allow_html=True)
+
+            # Convert the translated text to a Word document
+            word_output_file = "translated_text.docx"
+            convert_text_to_word_doc(translated_text, word_output_file)
+            # Provide a download link for the Word document
+            st.markdown(get_binary_file_downloader_html("Download Word Document", word_output_file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
