1a4b05e974c8e7f6cea542f85ae28db9cf814d4c
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-18 20:10:31 +0530

    Update id2.py

--- id2.py
+++ id2.py
@@ -1,37 +1,49 @@
 import streamlit as st
-import docx
+from gtts import gTTS
+import tempfile
+import os
+import base64
 
-def count_words_in_docx(docx_file):
-    try:
-        doc = docx.Document(docx_file)
+# ... (Rest of the code remains the same as in your original code)
 
-        total_words = 0  # Initialize the word count
-
-        for paragraph in doc.paragraphs:
-            # Count words in the current paragraph and add to the total
-            words_in_paragraph = len(paragraph.text.split())
-            total_words += words_in_paragraph
-
-        return total_words
-
-    except Exception as e:
-        return str(e), 0  # Return 0 words in case of an error
-
+# Define the Streamlit app
 def main():
-    st.title("Word Counter for DOCX")
+    st.image("jangirii.png", width=100)
+    st.subheader("Pray, Trust and Wait")
+    st.title("Text to Voice Converter")
 
-    uploaded_file = st.file_uploader("Upload a DOCX file", type=["docx"])
+    # Get user input
+    text = st.text_input("Enter text to convert:")
 
-    if uploaded_file is not None:
-        st.markdown("### Uploaded DOCX File:")
-        st.write(uploaded_file.name)
+    # Language selection options with full names
+    language_options = [f"{full_name} ({code})" for code, full_name in language_mapping.items()]
+    language = st.selectbox("Select Language", language_options)
 
-        total_words = count_words_in_docx(uploaded_file)
+    if st.button("Convert to Voice") and text:
+        # Extract language code from the selected option
+        selected_language_code = language.split(" ")[-1][1:-1]
 
-        if total_words:
-            st.markdown(f"### Word Count: {total_words} words")  # Display the word count
-        else:
-            st.warning("No words found in the DOCX file.")
+        # Create a temporary file to store the speech as an MP3 file
+        with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as temp_file:
+            temp_file_name = temp_file.name
 
+            # Convert text to speech and save as an MP3 file
+            convert_text_to_speech(text, temp_file_name, language=selected_language_code)
+
+        # Send the voice note
+        st.audio(temp_file_name, format='audio/mp3')
+
+        # Provide download button for the MP3 file
+        st.markdown(get_binary_file_downloader_html('download audio file', temp_file_name, 'audio/mp3'), unsafe_allow_html=True)
+
+        # Remove the temporary file
+        if temp_file_name:
+            os.remove(temp_file_name)
+
+        # Count and display the number of words in the input text
+        word_count = count_words_in_text(text)
+        st.write(f"Number of words in the input text: {word_count}")
+
+# Run the Streamlit app
 if __name__ == "__main__":
     main()
