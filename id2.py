2da8ddbfe03b846a0f2b8064e1199bfb7655bc6f
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-19 01:13:19 +0530

    Update id2.py

--- id2.py
+++ id2.py
@@ -86,7 +86,12 @@ def translate_text_with_fallback(text, target_language):
 
     # If MyMemory fails, use Google Translate
     st.warning("Falling back to Google Translate...")
-    return translate_text_google(text, target_language)
+    try:
+        return translate_text_google(text, target_language)
+    except Exception as e:
+        st.error(f"Google Translate error: {str(e)}")
+
+    return "Translation failed. Please try again later."
 
 
 # Function to count words in the text
