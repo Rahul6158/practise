54a4ab671660562c600fd5c0bd9ce6e0f4357e65
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-18 22:03:25 +0530

    Update id2.py

--- id2.py
+++ id2.py
@@ -4,43 +4,82 @@ from PIL import Image
 import pytesseract
 import PyPDF2
 import docx2txt
+from translate import Translator
 
-# Language mapping dictionary
+# Language mapping
 language_mapping = {
     "en": "English",
     "es": "Spanish",
     "fr": "French",
     "de": "German",
-    # Add more languages as needed
+    "it": "Italian",
+    "pt": "Portuguese",
+    "nl": "Dutch",
+    "hi": "Hindi",
+    "ja": "Japanese",
+    "ko": "Korean",
+    "zh-cn": "Simplified Chinese",
+    "ru": "Russian",
+    "ar": "Arabic",
+    "th": "Thai",
+    "tr": "Turkish",
+    "pl": "Polish",
+    "cs": "Czech",
+    "sv": "Swedish",
+    "da": "Danish",
+    "fi": "Finnish",
+    "el": "Greek",
+    "hu": "Hungarian",
+    "uk": "Ukrainian",
+    "no": "Norwegian",
+    "id": "Indonesian",
+    "vi": "Vietnamese",
+    "ro": "Romanian",
+    "bn": "Bengali",
+    "fa": "Persian",
+    "iw": "Hebrew",
+    "bg": "Bulgarian",
+    "ca": "Catalan",
+    "hr": "Croatian",
+    "sr": "Serbian",
+    "sk": "Slovak",
+    "sl": "Slovenian",
+    "lt": "Lithuanian",
+    "lv": "Latvian",
+    "et": "Estonian",
+    "is": "Icelandic",
+    "ga": "Irish",
+    "sq": "Albanian",
+    "mk": "Macedonian",
+    "hy": "Armenian",
+    "ka": "Georgian",
+    "mt": "Maltese",
+    "mr": "Marathi",
+    "ta": "Tamil",
+    "te": "Telugu",
+    "ur": "Urdu",
+    "ne": "Nepali",
+    "si": "Sinhala",
+    "km": "Khmer",
+    "lo": "Lao",
+    "my": "Burmese",
+    "jw": "Javanese",
+    "mn": "Mongolian",
+    "zu": "Zulu",
+    "xh": "Xhosa"
 }
-
-# Function to display language options and translate text
-def translate_text(text, target_language):
-    if target_language not in language_mapping:
-        st.warning("Invalid target language selected.")
-        return
-
-    st.write(f"Original Text ({language_mapping['en']}):")
-    st.write(text)
-
-    st.write(f"Translated Text ({language_mapping[target_language]}):")
-    st.write("Translation not supported in this demo.")  # You can add your translation logic here
-
 # Create a Streamlit app
 st.title("File Uploader with OCR and Translation")
 
 # Create an uploader widget
 uploaded_file = st.file_uploader("Upload a file", type=["docx", "pdf", "jpg", "jpeg", "png", "txt"])
 
-# Language selection widget
-target_language = st.selectbox("Select Target Language", list(language_mapping.values()))
-
 if uploaded_file is not None:
     file_extension = uploaded_file.name.split('.')[-1].lower()
     if file_extension == "docx":
         # Display DOCX content
         docx_text = docx2txt.process(uploaded_file)
-        translate_text(docx_text, target_language)
+        st.write(docx_text)
     elif file_extension == "pdf":
         # Display PDF content
         pdf_text = ""
@@ -48,19 +87,30 @@ if uploaded_file is not None:
         for page_num in range(len(pdf_reader.pages)):
             page = pdf_reader.pages[page_num]
             pdf_text += page.extract_text()
-        translate_text(pdf_text, target_language)
+        st.write(pdf_text)
     elif file_extension in ["jpg", "jpeg", "png"]:
         # Display image
         img = Image.open(uploaded_file)
         st.image(img, caption="Uploaded Image", use_column_width=True)
-
+        
         # Extract text from the image using pytesseract's built-in OCR
         text = pytesseract.image_to_string(img)
-        translate_text(text, target_language)
+        st.write("Text extracted from the image:")
+        st.write(text)
     elif file_extension == "txt":
         # Display TXT content
         txt_text = uploaded_file.read()
-        translate_text(txt_text, target_language)
+        st.write(txt_text)
+
+    # Language translation
+    translation_language = st.selectbox("Select a language for translation:", options=list(language_mapping.values()))
+    target_language = [key for key, value in language_mapping.items() if value == translation_language][0]
+    
+    if st.button("Translate"):
+        translator = Translator(to_lang=target_language)
+        translated_text = translator.translate(text)
+        st.write(f"Translated text to {translation_language}:")
+        st.write(translated_text)
 
 # Add some instructions for the user
 st.write("Supported file types: DOCX, PDF, JPG/JPEG, PNG, TXT")
