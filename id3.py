c9018f00da50ea94989188f8b36d928626390dba
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-18 11:16:41 +0530

    Update id3.py

--- id3.py
+++ id3.py
@@ -1,47 +1,44 @@
 import streamlit as st
 import docx
-from io import BytesIO
+from docx.shared import Length
 
-def find_non_string_objects(docx_file):
-    non_string_objects = []
-
+def remove_non_string_objects(docx_file):
     try:
         doc = docx.Document(docx_file)
-        cleaned_doc = docx.Document()  # Create a new DOCX document to store the cleaned content
+        new_doc = docx.Document()
 
         for element in doc.element.body:
             if isinstance(element, docx.oxml.text.paragraph.CT_P):
-                # Check if the element is a paragraph
-                new_paragraph = cleaned_doc.add_paragraph()  # Create a new paragraph in the cleaned document
+                # Process paragraphs and their runs
                 for run in element.runs:
                     if run.text.strip():  # Check if the text content is non-empty
-                        # Add runs with non-empty text to the cleaned paragraph
+                        new_paragraph = new_doc.add_paragraph()
                         new_run = new_paragraph.add_run(run.text)
-                        # Copy formatting (e.g., bold, italic) from the original run to the new run
                         new_run.bold = run.bold
                         new_run.italic = run.italic
-                        # You can copy other formatting attributes as needed
-                    else:
-                        # If the text content is empty, check for non-string objects (e.g., tables, images)
-                        for child in run._r:
-                            if not isinstance(child, docx.oxml.text.Run):
-                                non_string_objects.append(child)
+                        new_run.underline = run.underline
+                        new_run.font.size = run.font.size
+                        new_run.font.color.rgb = run.font.color.rgb
+                        # You can copy other run properties as needed
 
-            elif not isinstance(element, docx.oxml.text.Run):
-                # If the element is not a paragraph, check for non-string objects
-                non_string_objects.append(element)
+            elif isinstance(element, docx.oxml.text.Table):
+                # Handle tables (you can skip or process them as needed)
+                new_table = new_doc.add_table(rows=element.rows, cols=element.cols)
+                for i, row in enumerate(element.rows):
+                    for j, cell in enumerate(row.cells):
+                        new_table.cell(i, j).text = cell.text
+                        # You can copy cell properties as needed
 
-        cleaned_buffer = BytesIO()  # Create a buffer to store the cleaned document
-        cleaned_doc.save(cleaned_buffer)
-        cleaned_buffer.seek(0)
-
-        return non_string_objects, cleaned_buffer
+        # Save the modified document to a new file
+        new_doc.save("document_without_non_string.docx")
+        
+        return "document_without_non_string.docx"
 
     except Exception as e:
-        return str(e), None
+        return str(e)
 
 def main():
-    st.title("Remove Non-String Objects from DOCX and Download")
+    st.title("Non-String Object Remover in DOCX")
 
     uploaded_file = st.file_uploader("Upload a DOCX file", type=["docx"])
 
@@ -49,13 +46,13 @@ def main():
         st.markdown("### Uploaded DOCX File:")
         st.write(uploaded_file.name)
 
-        non_string_objects, cleaned_buffer = find_non_string_objects(uploaded_file)
+        result = remove_non_string_objects(uploaded_file)
 
-        if cleaned_buffer:
-            st.info("Non-string objects removed. You can download the cleaned DOCX file below.")
-            st.markdown(get_binary_file_downloader_html("Download Cleaned DOCX", cleaned_buffer, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
+        if isinstance(result, str):
+            st.success("Non-string objects removed successfully.")
+            st.markdown(get_binary_file_downloader_html("Download Modified DOCX", result, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
         else:
-            st.warning(f"Failed to process the DOCX file. Error: {non_string_objects}")
+            st.error("Failed to process the DOCX file. Error: " + result)
 
 if __name__ == "__main__":
     main()
