805cfc1e9d1afc2830832a0f4686cb5fe7d677a9
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-19 23:24:49 +0530

    Update id3.py

--- id3.py
+++ id3.py
@@ -195,7 +195,7 @@ def count_words(text):
     words = text.split()
     return len(words)
 
-# Main Streamlit app
+# Main Streamlit app# Main Streamlit app
 def main():
     st.image("jangirii.png", width=50)
     st.title("Text Translation and Conversion to Speech (Auto-detect language)")
@@ -255,11 +255,14 @@ def main():
             st.subheader('Select Target Language to Translate : ')
             target_language = st.selectbox("Select target language:", list(language_mapping.values()))
 
+            # Map the selected target language to its language code
+            target_language_code = [code for code, lang in language_mapping.items() if lang == target_language][0]
+
             # Check if text is not empty or None before attempting translation
             if text and len(text.strip()) > 0:
                 # Translate the extracted text
                 try:
-                    translated_text = translate_text_with_fallback(text, source_language, target_language)
+                    translated_text = translate_text(text, source_language, target_language_code)
                 except Exception as e:
                     st.error(f"Translation error: {str(e)}")
                     translated_text = None
@@ -277,20 +280,33 @@ def main():
             # Convert the translated text to speech
             if st.button("Convert to Speech and get Translated document"):
                 output_file = "translated_speech.mp3"
-                convert_text_to_speech(translated_text, output_file, language=target_language)
 
-                # Play the generated speech
-                audio_file = open(output_file, 'rb')
-                st.audio(audio_file.read(), format='audio/mp3')
+                # Ensure that target_language_code is a valid language code
+                if target_language_code not in gTTS.LANGUAGES:
+                    st.error(f"Unsupported language code: {target_language_code}")
+                else:
+                    convert_text_to_speech(translated_text, output_file, language=target_language_code)
 
-                # Provide a download link for the MP3 file
-                st.markdown(get_binary_file_downloader_html("Download Audio File", output_file, 'audio/mp3'), unsafe_allow_html=True)
+                    # Play the generated speech
+                    audio_file = open(output_file, 'rb')
+                    st.audio(audio_file.read(), format='audio/mp3')
 
-                # Convert the translated text to a Word document
-                word_output_file = "translated_text.docx"
-                convert_text_to_word_doc(translated_text, word_output_file)
-                # Provide a download link for the Word document
-                st.markdown(get_binary_file_downloader_html("Download Word Document", word_output_file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
+                    # Play the generated speech (platform-dependent)
+                    if os.name == 'posix':  # For Unix/Linux
+                        os.system(f"xdg-open {output_file}")
+                    elif os.name == 'nt':  # For Windows
+                        os.system(f"start {output_file}")
+                    else:
+                        st.warning("Unsupported operating system")
+
+                    # Provide a download link for the MP3 file
+                    st.markdown(get_binary_file_downloader_html("Download Audio File", output_file, 'audio/mp3'), unsafe_allow_html=True)
+
+                    # Convert the translated text to a Word document
+                    word_output_file = "translated_text.docx"
+                    convert_text_to_word_doc(translated_text, word_output_file)
+                    # Provide a download link for the Word document
+                    st.markdown(get_binary_file_downloader_html("Download Word Document", word_output_file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
