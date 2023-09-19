c0abc5ec983341cce57897f5e4049cfc69469c8a
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-19 10:02:34 +0530

    Update id3.py

--- id3.py
+++ id3.py
@@ -2,187 +2,112 @@ import streamlit as st
 import os
 import base64
 import docx2txt
-from googletrans import Translator
+from translate import Translator
 from gtts import gTTS
 import io
 from docx import Document
 from bs4 import BeautifulSoup
+from PIL import Image
+import PyPDF2
+import pytesseract
+import easyocr
+from PIL import Image
+from langdetect import detect  # Add this import for language detection
 
-# Function to extract text from a DOCX file
-def process_docx_text(docx_file, skip_lists=True):
-    # Extract text from the DOCX file
-    if skip_lists:
-        # Use custom function to remove lists
-        text = process_docx_text_without_lists(docx_file)
-    else:
-        text = docx2txt.process(docx_file)
-    return text
+# ... (Rest of the code remains the same)
 
-# Custom function to remove lists from DOCX text
-def process_docx_text_without_lists(docx_file):
-    doc = Document(docx_file)
-    text = ""
-    for paragraph in doc.paragraphs:
-        if not paragraph.style.name.startswith('•'):
-            text += paragraph.text + '\n'
-    return text
-
-# Function to translate text using Google Translate API
-def translate_text_google(text, target_language):
-    translator = Translator()
-    translated_text = translator.translate(text, dest=target_language)
-    return translated_text.text
-
-# Function to convert text to speech and save as an MP3 file
-def convert_text_to_speech(text, output_file, language='en'):
-    if text:
-        tts = gTTS(text=text, lang=language)
-        tts.save(output_file)
-
-# Function to generate a download link for a file
-def get_binary_file_downloader_html(link_text, file_path, file_format):
-    with open(file_path, 'rb') as f:
-        file_data = f.read()
-    b64_file = base64.b64encode(file_data).decode()
-    download_link = f'<a href="data:{file_format};base64,{b64_file}" download="{os.path.basename(file_path)}">{link_text}</a>'
-    return download_link
-
-# Function to convert translated text to a Word document
-def convert_text_to_word_doc(text, output_file):
-    doc = Document()
-    doc.add_paragraph(text)
-    doc.save(output_file)
-
-# Function to convert Word document to HTML
-def convert_word_doc_to_html(docx_file):
-    txt = docx2txt.process(docx_file)
-    soup = BeautifulSoup(txt, 'html.parser')
-    return soup.prettify()
-
-language_mapping = {
-    "en": "English",
-    "te": "Telugu",
-    "mr": "Marathi",
-    "ta": "Tamil",
-    "ur": "Urdu",
-    "hi": "Hindi",
-    "ja": "Japanese",
-    "ko": "Korean",
-    "ar": "Arabic",
-    "es": "Spanish",
-    "fr": "French",
-    "de": "German",
-    "it": "Italian",
-    "pt": "Portuguese",
-    "nl": "Dutch",
-    "zh-cn": "Simplified Chinese",
-    "ru": "Russian",
-    "th": "Thai",
-    "tr": "Turkish",
-    "pl": "Polish",
-    "cs": "Czech",
-    "sv": "Swedish",
-    "da": "Danish",
-    "fi": "Finnish",
-    "el": "Greek",
-    "hu": "Hungarian",
-    "uk": "Ukrainian",
-    "no": "Norwegian",
-    "id": "Indonesian",
-    "vi": "Vietnamese",
-    "ro": "Romanian",
-    "bn": "Bengali",
-    "fa": "Persian",
-    "iw": "Hebrew",
-    "bg": "Bulgarian",
-    "ca": "Catalan",
-    "hr": "Croatian",
-    "sr": "Serbian",
-    "sk": "Slovak",
-    "sl": "Slovenian",
-    "lt": "Lithuanian",
-    "lv": "Latvian",
-    "et": "Estonian",
-    "is": "Icelandic",
-    "ga": "Irish",
-    "sq": "Albanian",
-    "mk": "Macedonian",
-    "hy": "Armenian",
-    "ka": "Georgian",
-    "mt": "Maltese",
-    "ne": "Nepali",
-    "si": "Sinhala",
-    "km": "Khmer",
-    "lo": "Lao",
-    "my": "Burmese",
-    "jw": "Javanese",
-    "mn": "Mongolian",
-    "zu": "Zulu",
-    "xh": "Xhosa"
-}
+# Function to detect the language of the input text
+def detect_language(text):
+    try:
+        return detect(text)
+    except Exception as e:
+        st.warning(f"Language detection error: {str(e)}")
+        return "en"  # Default to English if language detection fails
 
 # Main Streamlit app
 def main():
     st.image("jangirii.png", width=50)
-    st.title("Text Translation and Conversion to Speech (English - other languages)")
+    st.title("Text Translation and Conversion to Speech (Multilingual Support)")
 
-    # Add a file uploader for DOCX files
-    uploaded_docx = st.file_uploader("Upload a DOCX file", type=["docx"])
+    # Add a file uploader for DOCX, PDF, images
+    uploaded_file = st.file_uploader("Upload a file", type=["docx", "pdf", "jpg", "jpeg", "png", "txt"])
 
-    if uploaded_docx is not None:
-        # Read the uploaded DOCX file and process its text content
-        docx_text = process_docx_text(uploaded_docx)
+    if uploaded_file is not None:
+        file_extension = uploaded_file.name.split('.')[-1].lower()
 
-        # Display the extracted text
-        st.subheader("Text Extracted from Uploaded DOCX:")
-        st.write(docx_text)
+        # Initialize text as None
+        text = None
 
-        target_language = st.selectbox("Select target language:", list(language_mapping.values()))
+        if file_extension == "docx":
+            # Display DOCX content
+            text = process_docx_text_without_lists(uploaded_file)
+        elif file_extension == "pdf":
+            # Display PDF content
+            pdf_text = process_pdf_text_without_lists(uploaded_file)
+            text = pdf_text
+        elif file_extension in ["jpg", "jpeg", "png"]:
+            # Display image
+            img = Image.open(uploaded_file)
+            st.image(img, caption="Uploaded Image", use_column_width=True)
 
-        # Check if the target language is in the mapping
-        target_language_code = [code for code, lang in language_mapping.items() if lang == target_language][0]
+            # Extract text from the image using custom function
+            image_bytes = uploaded_file.read()  # Read the image as bytes
+            extracted_text = extract_text_from_image(image_bytes)
+            st.write("Text extracted from the image:")
+            st.write(extracted_text)
+        elif file_extension == "txt":
+            # Display TXT content
+            txt_text = uploaded_file.read()
+            text = txt_text
 
-        # Translate the extracted text using Google Translate
-        try:
-            translated_text = translate_text_google(docx_text, target_language_code)
-        except Exception as e:
-            st.error(f"Translation error: {str(e)}")
-            translated_text = None
+        if text is not None:
+            st.subheader("Text Extracted from Uploaded File:")
+            st.write(text)
 
+            # Detect the source language of the input text
+            source_language = detect_language(text)
 
-        # Display translated text
-        if translated_text:
-            st.subheader(f"Translated text ({target_language}):")
-            st.write(translated_text)
-        else:
-            st.warning("Translation result is empty. Please check your input text.")
+            st.subheader(f"Source Language Detected: {language_mapping.get(source_language, 'Unknown')}")
 
-        # Convert the translated text to speech
-        if st.button("Convert to Speech and get Translated document"):
-            output_file = "translated_speech.mp3"
-            convert_text_to_speech(translated_text, output_file, language=target_language_code)
+            st.subheader('Select Target Language for Translation:')
+            target_language = st.selectbox("Select target language:", list(language_mapping.values()))
 
-            # Play the generated speech
-            audio_file = open(output_file, 'rb')
-            st.audio(audio_file.read(), format='audio/mp3')
-
-            # Play the generated speech (platform-dependent)
-            if os.name == 'posix':  # For Unix/Linux
-                os.system(f"xdg-open {output_file}")
-            elif os.name == 'nt':  # For Windows
-                os.system(f"start {output_file}")
+            # Check if text is not empty or None before attempting translation
+            if text and len(text.strip()) > 0:
+                # Translate the extracted text
+                try:
+                    translated_text = translate_text(text, source_language, target_language)
+                except Exception as e:
+                    st.error(f"Translation error: {str(e)}")
+                    translated_text = None
             else:
-                st.warning("Unsupported operating system")
+                st.warning("Input text is empty. Please check your document.")
+                translated_text = None
 
-            # Provide a download link for the MP3 file
-            st.markdown(get_binary_file_downloader_html("Download Audio File", output_file, 'audio/mp3'), unsafe_allow_html=True)
+            # Display translated text
+            if translated_text:
+                st.subheader(f"Translated text ({target_language}):")
+                st.write(translated_text)
+            else:
+                st.warning("Translation result is empty. Please check your input text.")
 
-            # Convert the translated text to a Word document
-            word_output_file = "translated_text.docx"
-            convert_text_to_word_doc(translated_text, word_output_file)
-            # Provide a download link for the Word document
-            st.markdown(get_binary_file_downloader_html("Download Word Document", word_output_file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
+            # Convert the translated text to speech
+            if st.button("Convert to Speech and Get Translated Document"):
+                output_file = "translated_speech.mp3"
+                convert_text_to_speech(translated_text, output_file, language=target_language)
+
+                # Play the generated speech
+                audio_file = open(output_file, 'rb')
+                st.audio(audio_file.read(), format='audio/mp3')
+
+                # Provide a download link for the MP3 file
+                st.markdown(get_binary_file_downloader_html("Download Audio File", output_file, 'audio/mp3'), unsafe_allow_html=True)
+
+                # Convert the translated text to a Word document
+                word_output_file = "translated_text.docx"
+                convert_text_to_word_doc(translated_text, word_output_file)
+                # Provide a download link for the Word document
+                st.markdown(get_binary_file_downloader_html("Download Word Document", word_output_file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
-    
