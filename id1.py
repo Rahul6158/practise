5b3a2f9bd184e2733c95b86bdce0799f89680393
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-21 22:09:28 +0530

    Update id1.py

--- id1.py
+++ id1.py
@@ -171,7 +171,6 @@ def count_words(text):
     words = text.split()
     return len(words)
 
-# Main Streamlit app
 def main():
     st.image("jangirii.png", width=50)
     st.title("Text Translation and Conversion to Speech (English - other languages)")
@@ -179,14 +178,12 @@ def main():
     # Add a file uploader for DOCX, PDF, images
     uploaded_file = st.file_uploader("Upload a file", type=["docx", "pdf", "jpg", "jpeg", "png", "txt"])
 
-    # Create session state to store translated text
-    session_state = SessionState.get(translated_text=None)
-
     if uploaded_file is not None:
         file_extension = uploaded_file.name.split('.')[-1].lower()
 
         # Initialize text as None
         text = None
+        translated_text = None  # Initialize translated text
 
         if file_extension == "docx":
             # Display DOCX content
@@ -223,47 +220,40 @@ def main():
                 st.warning("Warning: The document contains more than 5000 words, which may be too large for translation.")
                 return  # Exit the function if word count exceeds 5000
 
-            st.subheader('Select Language to Translate : ')
+            st.subheader('Select Language to Translate:')
             target_language = st.selectbox("Select target language:", list(language_mapping.values()))
 
-            # Check if text is not empty or None before attempting translation
-            if text and len(text.strip()) > 0:
-                # Translate the extracted text
-                if st.button("Translate"):
+            if st.button("Translate"):
+                # Check if text is not empty or None before attempting translation
+                if text and len(text.strip()) > 0:
+                    # Translate the extracted text
                     try:
                         translated_text = translate_text_with_fallback(text, target_language)
-                        session_state.translated_text = translated_text  # Store translated text in session state
                     except Exception as e:
                         st.error(f"Translation error: {str(e)}")
-                        session_state.translated_text = None
+                        translated_text = None
                 else:
-                    session_state.translated_text = None
-            else:
-                st.warning("Input text is empty. Please check your document.")
+                    st.warning("Input text is empty. Please check your document.")
 
-            # Display translated text
-            if session_state.translated_text is not None:
-                st.subheader(f"Translated text ({target_language}):")
-                st.write(session_state.translated_text)
+                # Display translated text
+                if translated_text:
+                    st.subheader(f"Translated text ({target_language}):")
+                    st.write(translated_text)
+                else:
+                    st.warning("Translation result is empty. Please check your input text.")
 
-    # Show the "Convert to Speech and get Translated document" button
-    if session_state.translated_text is not None:
-        if st.button("Convert to Speech and get Translated document"):
-            # Use the translated text from session state
-            translated_text = session_state.translated_text
+            if translated_text is not None:
+                if st.button("Convert to Speech and get Translated document"):
+                    # Get the target language code from language_mapping
+                    target_language_code = [code for code, lang in language_mapping.items() if lang == target_language][0]
 
-            if translated_text:
-                # Get the target language code from language_mapping
-                target_language_code = [code for code, lang in language_mapping.items() if lang == target_language][0]
+                    # Translate text using Google Translate
+                    try:
+                        translated_text = translate_text_with_google(translated_text, target_language_code)
+                    except Exception as e:
+                        st.error(f"Google Translate error: {str(e)}")
+                        return
 
-                # Translate text using Google Translate
-                try:
-                    translated_text = translate_text_with_google(translated_text, target_language_code)
-                except Exception as e:
-                    st.error(f"Google Translate error: {str(e)}")
-                    translated_text = None  # Set translated_text to None to handle the error
-
-                if translated_text is not None:
                     # Convert translated text to speech
                     output_file = "translated_speech.mp3"
                     convert_text_to_speech(translated_text, output_file, language=target_language_code)
